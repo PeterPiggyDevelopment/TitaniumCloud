95f33e01792b768f347918af4b4f7e1a457cb182
Kirill <deadwolfst@gmail.com>
2016-01-26 22:27:16 +0300

    server: added command find

--- src/Command.hs
+++ src/Command.hs
@@ -4,6 +4,7 @@ import Control.Concurrent.MVar
 import System.Exit(ExitCode(ExitSuccess))
 import System.Posix.Process(exitImmediately)
 import Data.List.Utils(strFromAL)
+import Data.List(isPrefixOf)
 import DataBase
 
 
@@ -13,7 +14,7 @@ initialHelp = "Hello! This is a TitaniumCloud server!\n" ++
 
 -- Help massage
 help = "If you want to print this help, type \"help\"\n" ++
-    "If you want to exit (:()), type \"exit\"\n" ++ 
+    "If you want to quit (:()), type \"quit\"\n" ++ 
     "If want to delete DataBase of users, type \"rmdb\"\n" ++
     "If you want to list the Data Base, type \"lsdb\"\n" ++
     "If you want to find user \"namename\" in Data Base, type \"fnusr\"\n" ++
@@ -35,15 +36,11 @@ procesCommands :: MVar [(String, Int)] -> IO ()
 procesCommands store = Prelude.getLine >>= 
         \com -> case com of
              "help" -> Prelude.putStrLn help
-             "exit" -> exitImmediately ExitSuccess
+             "quit" -> exitImmediately ExitSuccess
              "rmdb" -> Prelude.writeFile "DataBase" "" >> 
                  Prelude.putStrLn "Data Base removed"
              "lsdb" -> Prelude.readFile "DataBase" >>=
                  \db -> Prelude.putStrLn $ "Data Base: " ++ db
-             "fnusr" -> findUserInDB ("namename", "namename") False >>=
-                 \user -> case user of 
-                   Just u -> Prelude.putStrLn $ "Data Base has user " ++ fst u
-                   Nothing -> Prelude.putStrLn "Data Base has no users whith name \"namename\""
              "pst" -> readMVar store >>=
                  \st -> Prelude.putStrLn ("Statistics since server has been started:\n" ++ 
                     strFromAL st)
@@ -51,4 +48,11 @@ procesCommands store = Prelude.getLine >>=
                  \st -> Prelude.putStrLn ("Statistics since server has been started:\n" ++ 
                     "Authenticated users: " ++ show (fst (genShortStats st)) ++ "\n" ++
                     "Not authenticated users: " ++ show (snd (genShortStats st)))
-             _ -> Prelude.putStrLn "Invalid Invalidovich"
+             _ | "find" `isPrefixOf` com -> 
+                findUserInDB (drop 5 com, "") False >>=
+                     \usr -> case usr of
+                         Just (n, p) -> if drop 5 com == n 
+                             then putStrLn $ n ++ " " ++ p
+                             else  putStrLn "False"
+                         Nothing -> putStrLn "False"
+               | otherwise -> Prelude.putStrLn "Invalid Invalidovich"
